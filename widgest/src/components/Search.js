d3332c5af0d72a42a910ca833a98e913e780030c
Alan McCracken <acmccracken@gmail.com>
2020-09-14 10:57:13 -0500

    added dropdown

--- widgest/src/components/Search.js
+++ widgest/src/components/Search.js
@@ -27,6 +27,10 @@ const Search = () => {
               search();
             }
         }, 500);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
     }, [term]);
 
     const renderedResults = results.map((result) => {
